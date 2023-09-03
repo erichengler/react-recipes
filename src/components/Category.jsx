9f5ff43ae26ad8dfb52fce2db62bbd99e53c7f69
Erich Engler <erich.engler@hotmail.com>
2023-09-03 17:32:24 -0500

    built and styled nav links

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,31 +1,31 @@
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa';
 import { GiNoodles, GiChopsticks } from 'react-icons/gi';
-import { NavLink } from 'react-router-dom'
+import SLink from '../styles/SLink';
 import List from '../styles/List';
 
 function Category() {
 
     return (
         <List>
-            <NavLink to={'/cuisine/Italian'}>
+            <SLink  to={'/cuisine/Italian'}>
                 <FaPizzaSlice />
                 <h4>Italian</h4>
-            </NavLink>
+            </SLink >
 
-            <NavLink to={'/cuisine/American'}>
+            <SLink  to={'/cuisine/American'}>
                 <FaHamburger />
                 <h4>American</h4>
-            </NavLink>
+            </SLink >
 
-            <NavLink to={'/cuisine/Thai'}>
+            <SLink  to={'/cuisine/Thai'}>
                 <GiNoodles />
                 <h4>Thai</h4>
-            </NavLink>
+            </SLink >
 
-            <NavLink to={'/cuisine/Japanese'}>
+            <SLink  to={'/cuisine/Japanese'}>
                 <GiChopsticks />
                 <h4>Japanese</h4>
-            </NavLink>
+            </SLink >
         </List>
     );
 }
